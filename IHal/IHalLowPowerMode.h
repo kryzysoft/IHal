eadfefb7603bc8aacd2479957032f64c57be03ca
kryzysoft <kryzysoft@gmail.com>
2019-09-06 18:19:24 +0200

    First commit

---
+++ IHal/IHalLowPowerMode.h
@@ -0,0 +1,11 @@
+#ifndef I_HAL_LOW_POWER_MODE_H
+#define I_HAL_LOW_POWER_MODE_H
+
+class IHalLowPowerMode
+{
+  public:
+    virtual void Enter() = 0;
+    virtual ~IHalLowPowerMode(){};
+};
+
+#endif
